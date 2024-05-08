5745f00ad9c60f43a58553d0b23013ab3b4a8952
Wishwa <126236252+VishwaDilshara@users.noreply.github.com>
2024-05-08 22:05:17 +0530

    added api to server

--- frontend/App.js
+++ frontend/App.js
@@ -27,7 +27,7 @@ const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="SignUp">
+      <Stack.Navigator initialRouteName="Home">
         <Stack.Screen name="Home" component={Home} />
         {/* <Stack.Screen name="Home Page" component={HomePage} /> */}
 
